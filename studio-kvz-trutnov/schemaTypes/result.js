f3da2004394105e8a37a6ebb7b8b0c71c88e018d
Jan-Kadlec <jan.kadlec07@seznam.cz>
2026-02-02 21:58:18 +0100

    schema type pro results je správně

--- studio-kvz-trutnov/schemaTypes/result.js
+++ studio-kvz-trutnov/schemaTypes/result.js
@@ -1,45 +1,66 @@
 export default {
-  name: "result",
-  title: "Výsledky",
-  type: "document",
+  name: 'result',
+  title: 'Výsledky',
+  type: 'document',
   fields: [
     {
-      name: "title",
-      title: "Název soutěže",
-      type: "string",
+      name: 'title',
+      title: 'Název soutěže',
+      type: 'string',
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "date",
-      title: "Datum",
-      type: "date",
+      name: 'date',
+      title: 'Datum',
+      type: 'date',
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "location",
-      title: "Místo",
-      type: "string",
+      name: 'location',
+      title: 'Místo',
+      type: 'string',
     },
     {
-      name: "description",
-      title: "Popis / shrnutí",
-      type: "text",
+      name: 'description',
+      title: 'Popis / shrnutí',
+      type: 'text',
       rows: 4,
     },
     {
-      name: "results",
-      title: "Výsledková listina",
-      type: "array",
+      name: 'results',
+      title: 'Výsledková listina',
+      type: 'array',
       of: [
         {
-          type: "object",
+          type: 'object',
           fields: [
-            { name: "name", title: "Jméno", type: "string" },
-            { name: "position", title: "Umístění", type: "number" },
-            { name: "score", title: "Body", type: "string" },
+            {name: 'name', title: 'Jméno', type: 'string'},
+            {name: 'position', title: 'Umístění', type: 'number'},
+            {name: 'score', title: 'Body', type: 'string'},
           ],
         },
       ],
     },
+    {
+      name: 'proposition_file',
+      title: 'Propozice (PDF)',
+      type: 'file',
+      options: {
+        accept: 'application/pdf',
+      },
+    },
+    {
+      name: 'results_file',
+      title: 'Výsledky (PDF)',
+      type: 'file',
+      options: {
+        accept: 'application/pdf',
+      },
+    },
+    {
+      name: 'photos',
+      title: 'Fotografie (obrázek)',
+      type: 'image',
+    },
   ],
-};
+}
